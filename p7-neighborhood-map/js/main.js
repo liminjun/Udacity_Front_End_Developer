30501c8d84b61c5af08b5fe363674b4d7d239d2c
liminjun <v-leeli@hotmail.com>
2017-02-19 21:11:09 +0800

    update the codes of project7.

--- p7-neighborhood-map/js/main.js
+++ p7-neighborhood-map/js/main.js
@@ -18,32 +18,54 @@ var PlacesViewModel = function () {
   var self = this;
 
   self.places = [];
-  $.getJSON('api.data.json', function (respone) {
-    self.places = respone;
+  self.myCenter = "37.76680465,-122.41403674111";
 
-    self.places.forEach(function (place) {
-      addMarker(place);
-    });
+  $.ajax({
+    url: 'https://api.foursquare.com/v2/venues/search?ll=' + self.myCenter +
+    '&client_id=NONGGLXBKX5VFFIKKEK1HXQPFAFVMEBTRXBWJUPEN4K14JUE&client_secret=ZZDD1SLJ4PA2X4AJ4V23OOZ53UM4SFZX0KORGWP5TZDK4YYJ&v=20160101&limit=10',
+    dataType: "json",
+    success: function (result) {
+      if (result.meta.code == 200) {
+        
+        self.places = result.response.venues;
+        self.places.forEach(function (place) {
+          addMarker(place);
+        });
+      }
+    },
+    error: function (e) {
+      document.getElementById("error").innerHTML = "<h4>Get data from Foursquare's API  failed.</h4>";
+    }
   });
+
+  // $.getJSON('api.data.json', function (respone) {
+  //   self.places = respone;
+
+  //   self.places.forEach(function (place) {
+  //     addMarker(place);
+  //   });
+  // });
 
 
 }
 
-function getYelpInfo(name,url,snippet_image_url,snippet_text,address){
-  var yelpUrl="";
+function getYelpInfo(name, url, snippet_image_url, snippet_text, address) {
+  var yelpUrl = "";
 }
 function addMarker(place) {
-  var coordinate = place.location.coordinate;
+  var location=place.location;
+  var latitude=location.lat;
+  var longitude=location.lng;
+  var title= place.name;
 
   place.marker = new google.maps.Marker({
-    position: { lat: coordinate.latitude, lng: coordinate.longitude },
+    position: { lat: latitude, lng: longitude },
     map: map,
-    title: place.name,
-    yelpInfo:getYelpInfo(place.name,place.url,place.snippet_text,place.snippet_image_url,place.location.address)
+    title: title,
   });
 
-  place.marker.addListener("click",function(){
-    var that=this;
+  place.marker.addListener("click", function () {
+    var that = this;
     showMarkerInfo(that);
   });
 }
